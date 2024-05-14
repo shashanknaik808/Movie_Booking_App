502c763f85f9e138a3d6d4b2391ce5be6946fafa
Shashank Naik <shashanknaik808@gmail.com>
2024-05-14 17:49:44 +0530

    Implemented admin login functionality with password encryption

--- backend/controllers/admin_controller.js
+++ backend/controllers/admin_controller.js
@@ -31,3 +31,31 @@ module.exports.addAdmin = async (req, res, next) => {
     }
     return res.status(201).json({ admin });
 };
+
+module.exports.adminLogin = async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email && email.trim() === "" && !password && password.trim() === "") {
+        return res.status(422).json({ message: "Invalid Inputs" });
+    }
+    let existingAdmin;
+    try {
+        existingAdmin = await Admin.findOne({ email });
+    } catch (err) {
+        return console.log(err);
+    }
+    if (!existingAdmin) {
+        return res.status(400).json({ message: "Admin not found" });
+    }
+    const isPasswordCorrect = bcrypt.compareSync(
+        password,
+        existingAdmin.password
+    );
+
+    if (!isPasswordCorrect) {
+        return res.status(400).json({ message: "Incorrect Password" });
+    }
+
+    return res
+        .status(200)
+        .json({ message: "Authentication Complete" });
+};
