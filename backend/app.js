9fa32e6c056e2c6cfd6e937b7bde4caabd47c4b2
Shashank Naik <shashanknaik808@gmail.com>
2024-05-14 18:11:07 +0530

    Integrated movie router into the main application, enabling movie-related endpoints

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,17 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRouter = require("./routes/user_routes");
 const adminRouter = require('./routes/admin_routes');
-
+const movieRouter = require('./routes/movie_routes');
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 app.use("/user", userRouter);
-app.use("/admin", adminRouter)
+app.use("/admin", adminRouter);
+app.use("/movie", movieRouter);
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.dstwx2f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.gia1v3o.mongodb.net/movie_booking?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => app.listen(5000))
     .then(() => console.log("Database is connected! Listening to localhost 5000"))
     .catch((err) => console.log(err));
